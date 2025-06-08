4d7f902137f22f416444c7690875f471faad012a
SuperSteban <jestebansantti@gmail.com>
2025-06-08 15:52:09 -0700

    adds a more propper project structure to the project

--- src/routes/sup.ts
+++ src/routes/sup.ts
@@ -8,7 +8,7 @@ router.get("/", (req, res)=>{
      <html>
       <head><title>Hola</title></head>
       <body>
-        <h1>👋 Hola COPY YA ME FUI A DORMIR 😴😴😴</h1>
+        <h1>👋 Buenas Quiero Una ESPALDONA COMO LA DEL MIGELON😴😴😴</h1>
         <p>GRASHIA POR SALUDAR VALIO LA PENA</p>
         <img src=\"https://i.pinimg.com/736x/a4/82/88/a48288203ce23f43cffe6980fea1d411.jpg"\>
       </body>
